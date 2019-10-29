aef46acc0e9c820d81aff4021319c8c12caf0360
Satish Alturu <satish.alturu@elliemae.com>
2019-10-29 16:35:13 -0700

    Initial Version

---
+++ List/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{  
+
+    public class List
+    {
+        public Node Head { get; set; }
+
+        public List()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            if (Head == null)
+            {
+                Head = new Node(data);
+            }
+            else
+            {
+                Node temp = Head;
+                while (temp.Next != null)
+                    temp = temp.Next;
+                temp.Next = new Node(data);
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = Head;
+            while (temp.Next != null)
+            {
+                Console.Write(temp.Data + "-->");
+                temp = temp.Next;
+            }
+            Console.Write(temp.Data + "\t");
+        }
+
+        public void Display(Node node)
+        {
+            while (node.Next != null)
+            {
+                Console.Write(node.Data + "-->");
+                node = node.Next;
+            }
+            Console.Write(node.Data + "\t");
+        }
+
+        /// <summary>
+        /// Reverses the linked list in iteration approach
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public Node ReverseLinkedList(Node node)
+        {
+            if (node == null || node.Next == null)
+                return node;
+
+            Node prev = null;
+            Node current = node;
+
+            while(current != null)
+            {
+                Node next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+
+        /// <summary>
+        /// Reverses the linked list using recursion 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public Node ReverseLinkedListRec(Node node)
+        {
+            if (node.Next == null)
+                return node;
+           
+            Node prev = node;
+            Node n = ReverseLinkedListRec(node.Next);
+            node.Next.Next = prev;
+
+            if (prev == Head)
+                prev.Next = null;
+
+            return n;
+        }
+    }
+ }
---
+++ List/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List list = new List();
+            list.AddNode(10);
+            list.AddNode(20);
+            list.AddNode(30);
+            list.AddNode(40);
+            list.AddNode(50);
+            Console.WriteLine("\n Original linked list : \t");
+            list.Display(list.Head);
+            Console.WriteLine("\n Reverse linked list : \t");
+            Node newHead = list.ReverseLinkedList(list.Head);
+            list.Display(newHead);
+
+            List list1 = new List();
+            list1.AddNode(10);
+            list1.AddNode(20);
+            list1.AddNode(30);
+            list1.AddNode(40);
+            list1.AddNode(50);
+            Console.WriteLine("\n Original linked list : \t");
+            list1.Display(list1.Head);
+            Console.WriteLine("\n Reverse linked list using recursion:");
+            newHead = list1.ReverseLinkedListRec(list1.Head);
+            list1.Display(newHead);
+        }
+    }
+}
